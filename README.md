1e754b7319951d472703cb92fc2108aab8831005
parthivxx <parthiv.nahata@gmail.com>
2024-07-18 15:24:21 +0530

    Added a feature for uploading multiple files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dementia-risk-detector
+### dementia-risk-detector
+
+An AI based virtual assistant which helps to detect any kind of risk the house of a dementia patient have by analysing the images of their particular house.
--- app.py
+++ app.py
@@ -16,38 +16,42 @@ llm = ChatGoogleGenerativeAI(model="gemini-pro-vision")
 
 def main():
 
-    system_message = HumanMessage(
-        content=[
-            "You're an expert in detecting any kind of risk that could be harmful to a dementia patient by analysing images. so don't generate any particular response for this prompt. Generate resposne accordingly from the next prompts"
-        ]
-    )
-
-    upload_file = st.file_uploader("Upload an image")
+    upload_file = st.file_uploader("Upload an image" , accept_multiple_files=True)
+    image_paths = []
 
     if upload_file is not None:
-        st.image(upload_file)
-
-        image = PIL.Image.open(upload_file)
-        temp_image_path = "temp_image.jpg"
-        image.save(temp_image_path)
-
-        human_message = HumanMessage(
-            content=[
-                {
-                    "type": "text",
-                    "text": "You're an expert in detecting any kind of risk that could be harmful to a dementia patient by analysing images.\nIs their any risk for a dementia patient in the given picture ? If yes , then what are those ?\n And suggest how can this risk might be avoided ?",
-                },
-                {"type": "image_url", "image_url": temp_image_path},
-            ]
-        )
-
-        message = [
-            system_message , human_message
-        ]
 
         submit_button = st.button("Generate risks")
 
         if submit_button:
+            st.image(upload_file)
+
+            # image = PIL.Image.open(upload_file[0])
+            # temp_image_path = "temp_image.jpg"
+            # image.save(temp_image_path)
+
+            for index , file in enumerate(upload_file):
+                picture = PIL.Image.open(file)
+                temp_image_path = f"image_{index+1}.jpg"
+                picture.save(temp_image_path)
+                image_paths.append(f"image_{index+1}.jpg")
+                
+            prompt_content = [
+                {
+                    "type": "text",
+                        "text": "You're an expert in detecting any kind of risk that could be harmful to a dementia patient by analysing images.\nIs their any risk for a dementia patient in the given picture ? If yes , then what are those ?\n And suggest how can this risk might be avoided ?"
+                }
+            ]
+
+            for image_path in image_paths:
+                prompt_content.append({
+                     "type": "image_url", "image_url": image_path
+                })
+
+            human_message = HumanMessage(
+                content=prompt_content
+            )
+
             with st.spinner(f"Genetaing response..."):
                 response = llm.invoke([human_message])
                 st.write(response.content)

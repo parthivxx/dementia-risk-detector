8ae947a9ecca3ceec928508ec04f7546c1ab2126
parthivxx <parthiv.nahata@gmail.com>
2024-07-19 15:10:47 +0530

    Changed base model

--- app.py
+++ app.py
@@ -10,24 +10,26 @@ load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 st.title("DB HACKATHON")
 st.write("Welcome to the AI based risk detector for dementia patients!!")
-llm = ChatGoogleGenerativeAI(model="gemini-pro-vision")
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
+
+if 'intial_context' not in st.session_state:
+    st.session_state.initial_context = ""
 
 def main():
-
+    
     tab1,tab2 = st.tabs(["Risk detector from image" , "Risk detector from Online product"])
     with tab1:
         st.header("Tell Us About Yourself")
-        initial_context = ""
         role = st.radio("Are you the patient or a caregiver?", ('Patient', 'Caregiver'))
 
         if role == 'Patient':
             patient_name = st.text_input("Patient Name")
             patient_age = st.number_input("Patient Age", min_value=0, max_value=120, step=1)
-            
-            if st.button('Submit'):
+            submit = st.button("Submit")
+            if submit:
                 st.write(f"Thank you {patient_name} for providing your information.")
-                initial_context = f"""
+                st.session_state.initial_context = f"""
                                 Hi , I'm {patient_name} . I'm suffering 
                                 from dementia .My age is {patient_age} .
                             """
@@ -35,12 +37,13 @@ def main():
         elif role == 'Caregiver':
             caregiver_name = st.text_input("Caregiver Name")
             patient_relationship = st.text_input("Relationship with the Patient")
-            initial_context = f"""
+            
+            submit = st.button('Submit')
+            if submit:
+                st.session_state.initial_context = f"""
                                 Hi , I'm {caregiver_name} . I've someone who is suffering 
                                 from dementia currently. I'm their {patient_relationship}
                             """
-            
-            if st.button('Submit'):
                 st.write(f"Thank you {caregiver_name} for providing your information.")
 
         upload_file = st.file_uploader("Upload an image" , accept_multiple_files=True)
@@ -51,8 +54,7 @@ def main():
             
             if submit_button:
                 st.image(upload_file)
-                # st.write(upload_file)
-            
+                st.write(st.session_state.initial_context)
                 for index , file in enumerate(upload_file):
                     picture = PIL.Image.open(file)
 
@@ -64,8 +66,8 @@ def main():
                 prompt_content = [
                     {
                         "type": "text",
-                        "text":  initial_context + f"""
-                        You're an expert in detecting any kind of risk that could be harmful to a dementia patient by analyzing images. There are 2 images. Is there any risk for a dementia patient in the given pictures. Analyse each image and generate separate response for each image separately. If yes, then what are those? And suggest how this risk might be avoided? Generate a detailed report.
+                        "text":  st.session_state.initial_context + f"""
+                        You're an expert in detecting any kind of risk that could be harmful to a dementia patient by analyzing images. There are {len(upload_file)} images. Is there any risk for a dementia patient in the given pictures. Analyse each image and generate separate response for each image separately. If yes, then what are those? And suggest how this risk might be avoided? Generate a detailed report.
                         """
                     }
                 ]
